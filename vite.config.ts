2281dc27cbfa780b0d303f585794d7de122faadf
Pcezaral <94208599+Pcezaral@users.noreply.github.com>
2025-12-08 18:24:44 -0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,15 +1,17 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import path from 'path'
 
 export default defineConfig({
   plugins: [react()],
-  resolve: {
-    alias: {
-      '@': path.resolve(__dirname, './src')
-    }
+  css: {
+    postcss: {}
   },
   build: {
-    outDir: 'dist'
+    outDir: 'dist',
+    sourcemap: false
+  },
+  server: {
+    port: 5173,
+    host: true
   }
 })
