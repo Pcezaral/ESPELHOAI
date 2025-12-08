e242718e082f37ef678266ffc748195e8e9fed7a
Pcezaral <94208599+Pcezaral@users.noreply.github.com>
2025-12-08 18:04:54 -0300

    empty file for deletion

--- client/DELETE_ME.js
+++ client/DELETE_ME.js
@@ -1,19 +1,1 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
 
-export default defineConfig({
-  plugins: [react()],
-  build: {
-    outDir: 'dist',
-    sourcemap: false,
-    rollupOptions: {
-      output: {
-        manualChunks: undefined
-      }
-    }
-  },
-  server: {
-    port: 5173,
-    host: true
-  }
-})
