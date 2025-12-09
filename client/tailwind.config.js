88877c48e1d32f0621e9d5da37aafe2d243c3a43
Pcezaral <94208599+Pcezaral@users.noreply.github.com>
2025-12-08 22:59:59 -0300

    Update tailwind.config.js

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -2,8 +2,10 @@
 export default {
   content: [
     "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}"
+    "./src/**/*.{js,ts,jsx,tsx,vue,svelte,astro,mdx}",
   ],
-  theme: { extend: {} },
+  theme: {
+    extend: {},
+  },
   plugins: [],
 };
