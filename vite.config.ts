2b6933cfbcdfe274b17f45b1210e1be1fe7ce701
Pcezaral <94208599+Pcezaral@users.noreply.github.com>
2025-12-22 18:59:37 -0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,14 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import path from 'path'
 
 export default defineConfig({
   plugins: [react()],
-  build: {
-    outDir: 'dist'
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src'),
+      '@shared': path.resolve(__dirname, '../shared'),
+      '@server': path.resolve(__dirname, '../server')
+    }
   }
 })
