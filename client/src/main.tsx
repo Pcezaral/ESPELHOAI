eae0f76f5a81553a46307098a86d31edd9550d9e
Pcezaral <94208599+Pcezaral@users.noreply.github.com>
2025-12-23 13:33:26 -0300

    Update main.tsx

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,10 +1,16 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
+import "./index.css";
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
+const root = document.getElementById("root");
+
+if (!root) {
+  throw new Error("Root não encontrado");
+}
+
+ReactDOM.createRoot(root).render(
   <React.StrictMode>
     <App />
   </React.StrictMode>
-)
+);
